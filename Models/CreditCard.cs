1217df384e4f04810eb05d02a5a820dcace3d165
finmannager <netdigitalbrasil@gmail.com>
2024-12-02 15:35:00 -0300

    Update CreditCard.cs

--- Models/CreditCard.cs
+++ Models/CreditCard.cs
@@ -3,7 +3,7 @@ namespace CreditCardApp.Models
     public class CreditCard
 {
     public string CardName { get; set; } //= "Bradesco";
-    public decimal CreditLimit { get; set; } = 0;
+    public decimal CreditLimit { get; set; } // = 0;
     public decimal CurrentBalance { get; set; }
 
     public CreditCard() { }
